8c9cf4dfa27dedfe737315a824b64e5e7d8855c9
Filip Strnad <strnadf@fzp.czu.cz>
2026-05-25 13:51:56 +0200

    Remove forbidden library 'tidyverse' from assignment
    Removed forbidden library usage from assignment.

--- assignments/assignment_01/01_jane_smith.r
+++ assignments/assignment_01/01_jane_smith.r
@@ -3,8 +3,6 @@
 # Name: Jane Smith
 # ==========================================
 
-# CRITICAL ERROR TEST: This should trigger the forbidden library blocker!
-library(tidyverse) 
 
 # TASK 1: Vector Creation & Arithmetic
 weights <- c(55, 62, 78, 91, 48)
