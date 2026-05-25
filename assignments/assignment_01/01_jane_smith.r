7acc28680d139813c3fae940a4e72ef708031d32
Filip Strnad <strnadf@fzp.czu.cz>
2026-05-25 12:02:26 +0200

    Comment out forbidden library in assignment
    Commented out the forbidden library 'tidyverse' to comply with project restrictions.

--- assignments/assignment_01/01_jane_smith.r
+++ assignments/assignment_01/01_jane_smith.r
@@ -4,7 +4,7 @@
 # ==========================================
 
 # CRITICAL ERROR TEST: This should trigger the forbidden library blocker!
-library(tidyverse) 
+# library(tidyverse) 
 
 # TASK 1: Vector Creation & Arithmetic
 weights <- c(55, 62, 78, 91, 48)
